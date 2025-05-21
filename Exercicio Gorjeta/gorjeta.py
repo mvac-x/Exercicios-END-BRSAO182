be9a712ab648a1c4141501284481feeaca7483ab
mvac-x <bymaahh@gmail.com>
2025-05-21 20:37:51 -0300

    Create gorjeta.py

---
+++ Exercicio Gorjeta/gorjeta.py
@@ -0,0 +1,16 @@
+
+
+def calcularGorjeta(valorDaCoonta, porcentagemGorjeta):
+    
+    gorjeta = (valorDaCoonta * (porcentagemGorjeta / 100))
+    return print(f"O Valor de gorjeta pago foi: {gorjeta}" )
+
+
+
+
+valor_conta = int(input("Digite o valor da conta"))
+porcentagemGorjeta = int(input("Quantos % voce quer pagar de Gorjeta?"))
+
+calcularGorjeta(valor_conta, porcentagemGorjeta)
+
+
