d6828b79b2f06b400a68f9527f51a95238bb3d3c
mvac-x <bymaahh@gmail.com>
2025-05-21 21:36:09 -0300

    Update senhaforte.py

--- Exercicio Senha Forte/senhaforte.py
+++ Exercicio Senha Forte/senhaforte.py
@@ -1,1 +1,15 @@
 senha = input("Digite a senha ou sair: ")
+
+
+
+
+def verificarSenha(senha):
+    #tamnho da senha
+    if len(senha) < 8:
+        print("Senha muito curta")
+        return False
+
+    #se tem numero 
+    if not any(char.isdigit() for char in senha):
+        return False
+    return True
